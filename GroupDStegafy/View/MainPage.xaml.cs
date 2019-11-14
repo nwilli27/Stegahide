d9132d525948a246694d04e2f20b5095531c6d37
Jonathan Nicholl <jnicho10@my.westga.edu>
2019-11-14 16:26:55 -0500

    Added a (nonfunctional) user interface

---
+++ GroupDStegafy/View/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace GroupDStegafy.View
+{
+    /// <summary>
+    ///     Main page of the Stegafy application.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
