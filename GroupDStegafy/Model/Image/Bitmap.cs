2b5b35e64e3862cbccf59782d3320624ef03ac3e
Nolan Williams <nwilli27@my.westga.edu>
2019-11-14 13:53:37 -0500

    Initial commit. Created Bitmap object and Reader/Writer

---
+++ GroupDStegafy/Model/Image/Bitmap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace GroupDStegafy.Model
+{
+    /// <summary>
+    ///     Class holds the implementation for a Bitmap image
+    /// </summary>
+    class Bitmap
+    {
+
+        #region Properties
+
+        //TODO base implementation, accessibility may change based on what we need.
+        public double DpiX { get; set; }
+
+        public double DpiY { get; set; }
+
+        public WriteableBitmap Image { get; set; }
+
+        #endregion
+    }
+}
