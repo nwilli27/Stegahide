9d19a920068694d147bbc4dd4046d7c5753403da
Jonathan Taylor Nicholl <jnicho10@my.westga.edu>
2019-11-24 17:36:59 -0500

    Fixed terminator string appearing in decoded text

--- GroupDStegafy/Model/Text/TextDecodeUtility.cs
+++ GroupDStegafy/Model/Text/TextDecodeUtility.cs
@@ -16,7 +16,7 @@ namespace GroupDStegafy.Model.Text
         private const int TotalByteBits = 8;
         private const char ZeroBit = '0';
 
-        public const string DecodingStopIndicator = "#.-.-.-# ";
+        public const string DecodingStopIndicator = "#.-.-.-#";
 
         #endregion
 
@@ -69,12 +69,9 @@ namespace GroupDStegafy.Model.Text
         /// </returns>
         public static bool IsFinishedDecoding(string binaryMessage)
         {
-            //TODO temporary fix to odd remaining bits. FIX later
-            var stop = "#.-.-.-#";
-
             var message = binaryMessage.ConvertBinaryToString().Trim();
-            message = message.Substring(Math.Max(0, message.Length - stop.Length));
-            return message.Equals(stop);
+            message = message.Substring(Math.Max(0, message.Length - DecodingStopIndicator.Length));
+            return message.Equals(DecodingStopIndicator);
         }
 
         #endregion
