7359c790269e32facbb22420b38f89fa34bb01eb
Jonathan Taylor Nicholl <jnicho10@my.westga.edu>
2019-11-26 11:51:38 -0500

    Fixed source image not updating in UI after encoding

--- GroupDStegafy/ViewModel/MainPageViewModel.cs
+++ GroupDStegafy/ViewModel/MainPageViewModel.cs
@@ -330,7 +330,7 @@ namespace GroupDStegafy.ViewModel
                 }
             }
 
-            this.OnPropertyChanged(nameof(this.SourceBitmap));
+            this.OnPropertyChanged(nameof(this.SourceWriteableBitmap));
             this.EncodeCommand.OnCanExecuteChanged();
             this.DecodeCommand.OnCanExecuteChanged();
             this.CanSaveSource = true;
